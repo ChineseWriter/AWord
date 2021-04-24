f1696cd9da81f24fb1f4ada7d992d4330deab557
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2021-04-24 13:44:22 +0800

    进行准备工作。

---
+++ AWord/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :${NAME}.py
+# @Time      :${DATE} ${TIME}
+# @Author    :Raink
---
+++ Options.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :${NAME}.py
+# @Time      :${DATE} ${TIME}
+# @Author    :Raink
+
+
+if __name__ == "__main__":
+    run_code = 0
