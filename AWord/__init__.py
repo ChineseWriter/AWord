d99d09dbe68c58f9112e970dd42d0e37b788e695
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2021-04-29 21:31:25 +0800

    添加项目描述。

--- AWord/__init__.py
+++ AWord/__init__.py
@@ -3,6 +3,7 @@
 # @FileName  :__init__.py
 # @Time      :2021/4/24 13:45
 # @Author    :Amundsen Severus Rubeus Bjaaland
+"""初始化文件，导入Require.py文件中的"""
 
 from AWord.Require import URL
 from AWord.Require import SentenceMode
